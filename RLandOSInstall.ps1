21dc3db1996c696136ac727bee8d842e518e55f1
TheRodrike <137729421+TheRodrike@users.noreply.github.com>
2023-10-18 21:30:51 +0200

    Add files via upload

---
+++ RLandOSInstall.ps1
@@ -0,0 +1,168 @@
+#RLand OS Install V8
+Read-Host -Prompt 'Bienvenido al Instalador de RLand OS By TheRodrike
+Es Recomendable tener los drivers actualizados antes de continuar
+Pulse Enter'
+
+#Variables
+$title    = 'RLand OS'
+$choices  = '&Yes', '&No'
+$apps = 'Desea revisar las apps recomendadas para Instalar?'
+$navsocial = 'Desea Revisar las Aplicaciones de Navegacion y Social?'
+$gaming = 'Desea Revisar las Aplicaciones de Gaming?'
+$multimedia = 'Desea Revisar las Aplicaciones de Multimedia?'
+$productividad = 'Desea Revisar las Aplicaciones de Productividad?'
+$custom = 'Desea Revisar las Aplicaciones de Personalizacion?'
+$firefox = 'Desea Descargar Firefox Nigthly (Navegador)?'
+$steam = 'Desea Descargar Steam (Plataforma de Juegos)?'
+$kodi = 'Desea Descargar Kodi (Reproductor Multimedia)?'
+$discord = 'Desea Descargar Discord (Chats)?'
+$teamspeak = 'Desea Descargar TeamSpeak (Comunicacion)?'
+$vsc = 'Desea Descargar Visual Studio Code (Editor de Archivos)?'
+$sonar = 'Desea Descargar Sonar (Audio Profesional)?'
+$notion = 'Desea Descargar Notion (Ofimatica en la Nube)?'
+$playnite = 'Desea Descargar Playnite (Launcher)?'
+$7zip= 'Desea Descargar 7zip (Archivos Comprimidos)?'
+$we = 'Desea Revisar Wallpaper Engine (Desktop) (De Pago)?'
+$tb = 'Desea Revisar el github de TranslucentTB (Taskbar)?'
+
+#Directorios
+Set-Location $env:USERPROFILE
+New-Item -Path '.\RLandOS' -ItemType Directory -Force
+
+#Personalizar
+Invoke-WebRequest 'https://raw.githubusercontent.com/TheRodrike/RLandOS/main/RLandOS/RLandOS.jpg' -OutFile '.\RLandOS\RLandOS.jpg'
+New-ItemProperty -Path 'HKCU:Control Panel\Desktop' -Name 'WallPaper' -Value "%USERPROFILE%\RLandOS\RLandOS.jpg" -PropertyType String -Force
+
+#Optimizar
+Set-Service -name DiagTrack -startupType disabled
+Set-Service -name SysMain -startupType disabled
+Set-Service -name SCPolicySvc -startupType disabled
+Set-Service -name WbioSrvc -startupType disabled
+Set-Service -name DPS -startupType disabled
+Set-Service -name WerSvc -startupType disabled
+Set-Service -name SCardSvr -startupType disabled
+Set-Service -name tapisrv -startupType disabled
+Set-Service -name WalletService -startupType disabled
+
+Write-Host 'Configuracion del Sistema Finalizada'
+Read-Host -Prompt 'Pulse Enter'
+
+#Dependencias
+Read-Host -Prompt 'Pulse Enter para Descargar Ambas Dependencias'
+
+Start-Process 'https://aka.ms/vs/17/release/vc_redist.x64.exe'
+
+Read-Host -Prompt 'Pulse Enter para Descargar la Siguiente Dependencia'
+
+Start-Process 'https://aka.ms/vs/17/release/vc_redist.x86.exe'
+
+#Apps
+$decision = $Host.UI.PromptForChoice($title, $apps, $choices, 0)
+if ($decision -eq 0) {
+    $decision = $Host.UI.PromptForChoice($title, $navsocial, $choices, 0)
+    if ($decision -eq 0) {
+        $decision = $Host.UI.PromptForChoice($title, $firefox, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-central-l10n/Firefox%20Installer.es-ES.exe'
+        } else {
+            Write-Host 'No'
+        }
+        $decision = $Host.UI.PromptForChoice($title, $discord, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86'
+        } else {
+            Write-Host 'No'
+        }
+        $decision = $Host.UI.PromptForChoice($title, $teamspeak, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://teamspeak.com/es/downloads/'
+        } else {
+            Write-Host 'No'
+        }
+    } else {
+        Write-Host 'No'
+    }
+    $decision = $Host.UI.PromptForChoice($title, $gaming, $choices, 0)
+    if ($decision -eq 0) {
+        $decision = $Host.UI.PromptForChoice($title, $steam, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe'
+        } else {
+            Write-Host 'No'
+        }
+        $decision = $Host.UI.PromptForChoice($title, $sonar, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://steelseries.com/gg/downloads/gg/latest/windows'
+        } else {
+            Write-Host 'No'
+        }
+        $decision = $Host.UI.PromptForChoice($title, $playnite, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://playnite.link/download/PlayniteInstaller.exe'
+        } else {
+            Write-Host 'No'
+        }
+    } else {
+        Write-Host 'No'
+    }
+    $decision = $Host.UI.PromptForChoice($title, $multimedia, $choices, 0)
+    if ($decision -eq 0) {
+        $decision = $Host.UI.PromptForChoice($title, $kodi, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://kodi.tv/download/windows'
+        } else {
+            Write-Host 'No'
+        }
+    } else {
+        Write-Host 'No'
+    }
+    $decision = $Host.UI.PromptForChoice($title, $productividad, $choices, 0)
+    if ($decision -eq 0) {
+        $decision = $Host.UI.PromptForChoice($title, $notion, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://notion.so/desktop/windows/download'
+        } else {
+            Write-Host 'No'
+        }
+        $decision = $Host.UI.PromptForChoice($title, $vsc, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user'
+        } else {
+            Write-Host 'No'
+        }
+        $decision = $Host.UI.PromptForChoice($title, $7zip, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://www.7-zip.org/download.html'
+        } else {
+            Write-Host 'No'
+        }
+    } else {
+        Write-Host 'No'
+    }
+    $decision = $Host.UI.PromptForChoice($title, $custom, $choices, 0)
+    if ($decision -eq 0) {
+        $decision = $Host.UI.PromptForChoice($title, $we, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://store.steampowered.com/app/431960/Wallpaper_Engine'
+        } else {
+            Write-Host 'No'
+        }
+        $decision = $Host.UI.PromptForChoice($title, $tb, $choices, 0)
+        if ($decision -eq 0) {
+            Start-Process 'https://github.com/TranslucentTB/TranslucentTB'
+        } else {
+            Write-Host 'No'
+        }
+    } else {
+        Write-Host 'No'
+    }
+} else {
+    Write-Host 'No'
+}
+
+#Finalizar
+Write-Host 'Gracias por Instalar RLand OS'
+Read-Host -Prompt 'Pulse Enter para Finalizar'
+
+#Reiniciar
+shutdown /r /t 0
